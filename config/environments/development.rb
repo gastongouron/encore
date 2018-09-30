8a42ebe4f25f49e3a1377590603301fe0a7afd5e
workstation <workstation@workstations-MacBook-Pro.local>
2018-09-30 18:19:34 +0200

    Update developement port for mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
+    address: 'smtp://localhost',
+    port: 1025,
     domain: 'gmail.com',
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD'],
