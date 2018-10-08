19958bd8f571bdbca8467cf3538f091cab3c5aa3
workstation <workstation@workstations-MacBook-Pro.local>
2018-10-08 19:51:36 +0200

    Temporery remove confirmable from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, 
     :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
+    # :confirmable,
   has_many :reviews
   has_many :artists, through: :reviews
 
