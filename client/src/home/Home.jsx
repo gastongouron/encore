1758e23ed0db31716463a9de7ad58faa76a2dad6
workstation <workstation@workstations-MacBook-Pro.local>
2018-12-26 16:21:09 +0100

    update home

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -97,12 +97,21 @@ const block2 = {
 }
 
 class Home extends Component {
-   constructor(props){
-        super(props);
-      }
 
+  constructor(props){
+    super(props);
+  }
 
   render () {
+    if(this.props.currentUser && this.props.currentUser.isLoggedIn){
+        return (
+          <div>
+            <br/>Last Blog post: titre
+            <br/>Top rated artists
+            <br/>Top rated users
+          </div>
+        );
+      }else{
     return (
       <div style={rootz}>
 
@@ -153,19 +162,19 @@ class Home extends Component {
               <Grid container style={featuresBlock}>
                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
                   <div style={{textAlign: "center", color: "black"}}>
-                    some artist
+                    some topranked artist
                   </div>
                 </Grid>
 
                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
                   <div style={{textAlign: "center", color: "black"}}>
-                    some artist
+                    some topranked artist
                   </div>
                 </Grid>
 
                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
                   <div style={{textAlign: "center", color: "black"}}>
-                    some artist
+                    some topranked artist
                   </div>
                 </Grid>
               </Grid>
@@ -180,104 +189,10 @@ class Home extends Component {
       </div>
 
       )
+    }
   }
 }
 
-// const Home = ({currentUser, locales, auth: {AuthLinks}, initLocales}) => {
-
-//   if(currentUser && currentUser.isLoggedIn){
-//     return (
-//       <div>
-//         <br/>Last Blog post: titre
-//         <br/>Top rated artists
-//         <br/>Top rated users
-//       </div>
-//     );
-//   }else{
-//     return (  
-//           <div style={rootz}>
-
-
-//             <Grid container style={header}>              
-//               <Grid style={headerBlock} item xs={12} sm={12} md={12}>
-//                 <h1 style={hashtag}>#Hashtag</h1>
-//                 <h1 style={baseline}>baseline</h1>
-//                 <h1 style={hello}>{locales.locales.homeHello}</h1>
-//                 <Link to="/users/getstarted" style={{float: "left"}}>
-//                   <RaisedButton label={locales.locales.getStarted}></RaisedButton>
-//                 </Link>
-
-//               </Grid>
-//             </Grid>
-
-//             <div style={features}>              
-//               <Grid container style={featuresBlock}>
-//                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
-//                   <div style={{textAlign: "center"}}>
-//                     <img style={featureItem} src={Star} /><br/>
-//                     <span style={legend}> Donne et consulte des<br/> avis sur des shows</span>
-//                   </div>
-//                 </Grid>
-
-//                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
-//                   <div style={{textAlign: "center"}}>
-//                     <img style={featureItem} src={Camera} /><br/>
-//                     <span style={legend}> Donne et consulte des<br/> avis sur des shows</span>
-//                   </div>
-//                 </Grid>
-
-//                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
-//                   <div style={{textAlign: "center"}}>
-//                     <img style={featureItem} src={Face} /><br/>
-//                     <span style={legend}> Donne et consulte des<br/> avis sur des shows</span>
-//                   </div>
-//                 </Grid>
-//               </Grid>
-//             </div>
-
-//             <Grid container style={block2}>     
-//               <Grid item style={{textAlign: 'center', margin: '0 auto'}}>
-//                 <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
-//               </Grid>
-//             </Grid>
-
-//             <div style={features}>              
-//               <Grid container style={featuresBlock}>
-//                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
-//                   <div style={{textAlign: "center"}}>
-//                     <img style={featureItem} src={Star} /><br/>
-//                     <span style={legend}> Donne et consulte des<br/> avis sur des shows</span>
-//                   </div>
-//                 </Grid>
-
-//                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
-//                   <div style={{textAlign: "center"}}>
-//                     <img style={featureItem} src={Camera} /><br/>
-//                     <span style={legend}> Donne et consulte des<br/> avis sur des shows</span>
-//                   </div>
-//                 </Grid>
-
-//                 <Grid style={{paddingTop: 20}} item xs={12} sm={4} md={4}>
-//                   <div style={{textAlign: "center"}}>
-//                     <img style={featureItem} src={Face} /><br/>
-//                     <span style={legend}> Donne et consulte des<br/> avis sur des shows</span>
-//                   </div>
-//                 </Grid>
-//               </Grid>
-//             </div>
-
-//         <MessengerCustomerChat
-//             pageId="826064754401701"
-//             appId="1351728908291385"
-//             language={locales.locales._language === 'en' ? "en_US" : "fr_FR"}
-//           />
-
-//       </div>
-//     );
-//   }
-
-
-// };
 
 const mapStateToProps = state => {
   initLocales();
