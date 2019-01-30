960cf32e49050b28f028aa7cccaaaad0d2fbe34e
workstation <workstation@workstations-MacBook-Pro.local>
2019-01-30 19:57:18 +0100

    wip

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -426,9 +426,30 @@ class Home extends Component {
               </Grid>
             </Grid>
 
+
+
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v3.2'
+    });
+  };
+
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
         <MessengerCustomerChat
             minimized={true}
-            pageId="2079915298988137"
+            pageId="826064754401701"
             appId="1351728908291385"
             language={this.props.locales.locales._language === 'en' ? "en_US" : "fr_FR"}
           /> 
